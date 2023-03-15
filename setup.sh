d054fccb2edfffd41beea1f2fc19e84890b96638
Y0h4n3s <yohaksa80@gmail.com>
2023-03-15 10:42:11 +0300

    bump ethers to 2.0.0

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ source "$HOME/.cargo/env"
 source "./garb/.env"
 sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
-wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+wget -O - https://debian.neo4j.com/neotechnology.gpg.key --no-check-certificate | sudo apt-key add -
 echo 'deb https://debian.neo4j.com stable latest' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
-sudo apt-get update
+apt -o "Acquire::https::Verify-Peer=false" update
 sudo apt-get install neo4j=1:5.4.0 -y
